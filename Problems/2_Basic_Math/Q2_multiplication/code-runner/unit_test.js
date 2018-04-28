69a4b19534b854e9f0a6464534b5b335080e3a71
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-04-29 06:54:40 +0800

    Add

--- Problems/2_Basic_Math/Q2_multiplication/code-runner/unit_test.js
+++ Problems/2_Basic_Math/Q2_multiplication/code-runner/unit_test.js
@@ -1,5 +1,5 @@
 module.exports = {
-    testCodeString : "Return the sum of integers",  // Description of the Tape test output
+    testCodeString : "Return the product of two integers",  // Description of the Tape test output
     testCases: [[1, 2], [0, 0], [-1, 1], [-1, -1]], // Test cases
     answers: [2, 0, -1, 1]                          // Answers to each test case
 };
---
+++ Problems/4_Conditional/Q1_even_odd/problem.js
@@ -0,0 +1,17 @@
+/*
+Write a function even_odd that takes one integer as an input and checks whether the
+given integer is even or odd. Display the result.
+
+Input: An integer
+Output: "Even" / "Odd"
+
+*/
+
+function even_odd(num){
+    // Your solution
+
+}
+
+module.exports = {
+    mainFunc: even_odd
+}
