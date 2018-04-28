a7f2e4dd8b98ffff90a18450dd4de6f426573b9a
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-04-29 06:55:41 +0800

    Update

--- Problems/4_Conditional/Q1_even_odd/code-runner/unit_test.js
+++ Problems/4_Conditional/Q1_even_odd/code-runner/unit_test.js
@@ -1,5 +1,5 @@
 module.exports = {
-    testCodeString : "Return the sum of integers",  // Description of the Tape test output
+    testCodeString : "Return 'Even' or 'Odd'",  // Description of the Tape test output
     testCases: [0, -2, 2, 1, -3],                        // Test cases
     answers: ["Even", "Even", "Even", "Odd", "Odd"]                      // Answers to each test case
 };
