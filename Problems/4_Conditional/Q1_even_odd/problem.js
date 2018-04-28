eb16b73662810e00c2235f34c825c88e8cb5d15c
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-04-29 07:18:03 +0800

    Test: Effect of Modifying master(problem) branch on student's branch

--- Problems/4_Conditional/Q1_even_odd/problem.js
+++ Problems/4_Conditional/Q1_even_odd/problem.js
@@ -1,6 +1,6 @@
 /*
-Write a function even_odd that takes one integer as an input and checks whether the
-given integer is even or odd. Display the result.
+Write a function even_odd that takes one integer as an input and returns whether the
+given integer is "Even" or "Odd".
 
 Input: An integer
 Output: "Even" / "Odd"
@@ -9,7 +9,12 @@ Output: "Even" / "Odd"
 
 function even_odd(num){
     // Your solution
-
+    if (num % 2 === 0){
+        return "Even";
+    }
+    else {
+        return "Odd";
+    }
 }
 
 module.exports = {
