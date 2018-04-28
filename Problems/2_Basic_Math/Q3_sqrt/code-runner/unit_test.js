181570cd52096853d0c0b61808a1a18ead58d911
yongzx <zhengxin.yong@minerva.kgi.edu>
2018-04-29 07:37:02 +0800

    Add new question

---
+++ Problems/2_Basic_Math/Q3_sqrt/code-runner/unit_test.js
@@ -0,0 +1,5 @@
+module.exports = {
+    testCodeString : "Return the square root of an integer",  // Description of the Tape test output
+    testCases: [0, 1, 4, 16, -1], // Test cases
+    answers: [0, 1, 2, 4, null]                          // Answers to each test case
+};
---
+++ Problems/2_Basic_Math/Q3_sqrt/problem.js
@@ -0,0 +1,18 @@
+/*
+Create a function that finds the square root of an integer. Return null if the
+square root of the integer is not a real number.
+
+Input: An integers
+Output: The square root of the integer
+*/
+
+function squareRoot(num){
+    // Your solution
+
+}
+
+// Do not delete
+// export the written function to be checked by code runner
+module.exports = {
+    mainFunc: squareRoot
+}
